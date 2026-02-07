d3c261c66406edf979d24cc3ceaaa17188301bf5
Robert Manders <github.powdered589@passmail.net>
2026-02-07 19:26:20 +0000

    Delete all the things.

--- backend/auth/token.go
+++
@@ -1,8 +0,0 @@
-package auth
-
-type Token struct {
-	AccessToken  string `json:"access_token"`
-	TokenType    string `json:"token_type"`
-	RefreshToken string `json:"refresh_token"`
-	ExpiresIn    int64  `json:"expires_in"`
-}
